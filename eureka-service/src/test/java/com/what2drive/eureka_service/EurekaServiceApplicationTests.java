650770c5b296143a7df7702ffe8cefa60e609645
Leo <leo.ertuna@gmail.com>
2019-08-11 00:38:52 +0300

    Eureka service

---
+++ eureka-service/src/test/java/com/what2drive/eureka_service/EurekaServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.what2drive.eureka_service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaServiceApplicationTests {
+    @Test
+    public void contextLoads() {
+
+    }
+}
